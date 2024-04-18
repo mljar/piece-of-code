0483e14c67aeb0e6c24c7017ca23a9b69ed26879
Piotr <pplonski86@gmail.com>
2024-04-18 17:10:02 +0200

    version 0.2.0

--- extension/build.sh
+++ extension/build.sh
@@ -4,5 +4,5 @@ jlpm run build
 
 python -m build
 
-cp dist/pieceofcode-0.1.0-py3-none-any.whl ../../studio/env_installer/extras/
+cp dist/pieceofcode-0.2.0-py3-none-any.whl ../../studio/env_installer/extras/
 
