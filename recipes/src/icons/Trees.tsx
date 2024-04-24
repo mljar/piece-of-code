06d1904b74274f86f5a413152ff86b6f16699b2f
Piotr <pplonski86@gmail.com>
2024-04-24 15:06:48 +0200

    add tree icons

---
+++ recipes/src/icons/Trees.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { IconProps } from "./props";
+
+export const TreeIcon: React.FC<IconProps> = ({
+  size = 24,
+  color = "currentColor",
+  className = "",
+}: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    width={size}
+    height={size}
+    viewBox={`0 0 ${size} ${size}`}
+    strokeWidth="2"
+    stroke={color}
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M16 5l3 3l-2 1l4 4l-3 1l4 4h-9" />
+    <path d="M15 21l0 -3" />
+    <path d="M8 13l-2 -2" />
+    <path d="M8 12l2 -2" />
+    <path d="M8 21v-13" />
+    <path d="M5.824 16a3 3 0 0 1 -2.743 -3.69a3 3 0 0 1 .304 -4.833a3 3 0 0 1 4.615 -3.707a3 3 0 0 1 4.614 3.707a3 3 0 0 1 .305 4.833a3 3 0 0 1 -2.919 3.695h-4z" />
+  </svg>
+);
