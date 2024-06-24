1a5872036de54ee295cd1efab4e6a1894424246e
Piotr <pplonski86@gmail.com>
2024-06-24 10:13:47 +0200

    fix delete file recipe (#23)

--- recipes/src/recipes/python/fileDelete.tsx
+++ recipes/src/recipes/python/fileDelete.tsx
@@ -13,13 +13,13 @@ export const FileDelete: React.FC<IRecipeProps> = ({
   metadata,
   setMetadata,
 }) => {
-  const [myFile, setMyFile] = useState("file_name");
+  const [myFile, setMyFile] = useState("file_path");
 
   useEffect(() => {
-    let src = `# open a file in write mode\n`;
+    let src = `# check if file path exists\n`;
     src += `if os.path.isfile(r"${myFile}"):\n`;
     src += `    # file exists, delete it\n`;
-    src += `    os.remove(r"${myFile})\n`;
+    src += `    os.remove(r"${myFile}")\n`;
     src += `else:\n`;
     src += `    print("File not found")`;
 
@@ -61,11 +61,13 @@ export const FileDeleteRecipe: IRecipe = {
   name: "Delete file",
   longName: "Delete file in Python",
   parentName: "Python",
-  description: "Check if file exists and if yes, then delete file.",
-  shortDescription: "Check if file exists and if yes, then delete file",
+  description: `Use Python to automatically delete file from file system using os module and remove function. This code additionally checks if file exists before calling remove on file path.`,
+  shortDescription: 
+`Use Python to automatically delete file from file system using os module and remove function. This code additionally checks if file exists before calling remove on file path.`  
+  ,
   codeExplanation: `
 1. Check if file exists in the file system.
-2. Delete file if present.
+2. Delete file if provided file path exists.
   `,
   tags: ["file", "delete", "remove"],
   ui: FileDelete,
