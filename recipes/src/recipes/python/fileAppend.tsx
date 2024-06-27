0e520192d033681215ed37f5b3250c120691ef0e
Piotr <pplonski86@gmail.com>
2024-06-27 14:27:10 +0200

    small fix

--- recipes/src/recipes/python/fileAppend.tsx
+++ recipes/src/recipes/python/fileAppend.tsx
@@ -82,7 +82,7 @@ export const FileAppendRecipe: IRecipe = {
   parentName: "Python",
   description:
     "You can append any content to file using Python. Python built-in functions `open()` and `read()` will help you automate your workflows. Files can be edited as text or binary format.",
-  shortDescription:  "You can append any content to file using Python. Python built-in functions `open()` and `read()` will help you automate your workflows. Files can be edited as text or binary format.",,
+  shortDescription:  "You can append any content to file using Python. Python built-in functions `open()` and `read()` will help you automate your workflows. Files can be edited as text or binary format.",
   codeExplanation: `
 1. Open file in append mode.
 2. Write a new content at the end of file.
