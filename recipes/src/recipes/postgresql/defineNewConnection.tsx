fc4b60edbaf1a249893cf5cd9e94acb97b734f7a
a-szulc <adamszulc17@gmail.com>
2024-08-16 09:23:10 +0200

    remove unused import and commented code (#67)

--- recipes/src/recipes/postgresql/defineNewConnection.tsx
+++ recipes/src/recipes/postgresql/defineNewConnection.tsx
@@ -21,26 +21,7 @@ export const DefineNewConnection: React.FC<IRecipeProps> = ({
     const [port, setPort] = useState("5432");
 
     useEffect(() => {
-        // let src = `connection_name = "${conn}"\n`;
-        // src += `database_name = "${dbname}"\n`;
-        // src += `username = "${username}"\n`;
-        // src += `password = "${password}"\n`;
-        // src += `host_address = "${host_address}"\n`;
-        // src += `port = "${port}"\n\n`;
-        // src += `mode = "a" if os.path.exists(".env") else "w"\n`;
-        // src += `# open file and write variable\n`;
-        // src += `with open(".env", mode) as fout:\n`;
-        // src += `    fout.write("POSTGRES_CONNECTION_NAME=connection_name")\n`;
-        // src += `	fout.write("POSTGRES_DB_NAME=database_name"\n`;
-        // src += `	fout.write("POSTGRES_USERNAME=username")\n`;
-        // src += `	fout.write("POSTGRES_PASSWORD=password")\n`;
-        // src += `	fout.write("POSTGRES_HOST_ADDRESS=host_address")\n`;
-        // src += `	fout.write("POSTGRES_PORT=port")\n`;
-        // src += `print("Secret saved in .env file")\n`;
-        // src += `print("Please remove this code cell and save notebook, be safe!")`;
-
         let src = `print("Database connection credentials stored in .env file successfully.")`;
-
         setCode(src);
 
         if (setEnv) {
@@ -52,14 +33,11 @@ export const DefineNewConnection: React.FC<IRecipeProps> = ({
                 ["POSTGRES_PORT", port]
             ]);
         }
-        setPackages(["import os"]);
+
+        setPackages([""]);
+
         if (setMetadata) {
             setMetadata({
-                // dbname,
-                // username,
-                // password,
-                // host_address,
-                // port,
                 docsUrl: DOCS_URL,
             });
         }
@@ -68,11 +46,6 @@ export const DefineNewConnection: React.FC<IRecipeProps> = ({
     useEffect(() => {
         if (metadata) {
             if ("mljar" in metadata) metadata = metadata.mljar;
-            // if (metadata["dbname"] !== undefined) setDBName(metadata["dbname"]);
-            // if (metadata["username"] !== undefined) setUsername(metadata["username"]);
-            // if (metadata["password"] !== undefined) setPassword(metadata["password"]);
-            // if (metadata["host_address"] !== undefined) setHostAddress(metadata["host_address"]);
-            // if (metadata["port"] !== undefined) setPort(metadata["port"]);
         }
     }, [metadata]);
 
@@ -118,22 +91,6 @@ export const DefineNewConnection: React.FC<IRecipeProps> = ({
                     setName={setPort}
                 />
             </div>
-            {/*
-            <div
-                className="poc-mt-2 poc-p-4 poc-mb-4 poc-text-base poc-text-red-800 poc-rounded-lg poc-bg-red-50 dark:poc-bg-gray-800 dark:poc-text-red-400"
-                role="alert"
-            >
-                <span className="poc-font-semibold poc-text-xl">
-                    Please remove this code cell after execution!
-                </span>
-                <br />
-                This cell runs code that saves variables in the <b>.env</b> file in your
-                current directory. It is used to keep secrets. Secrets can't be stored
-                in the code for security reasons. If the cell is executed successfully,
-                please check that you have a <b>.env</b> file, and you can safely remove
-                this cell and save the notebook.
-            </div>
-            */}
         </div>
     );
 };
